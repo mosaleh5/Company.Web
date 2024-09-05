63316f543ff3e2d612176e35531efc08a91e1251
mosaleh55 <www.mosaleh@gmail.com>
2024-09-05 03:35:40 +0300

    UnitOfWork

---
+++ Company.Repository/Repositories/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using Company.Data.Contexts;
+using Company.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Repository.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public CompanyDbContext _Context;
+        public UnitOfWork(CompanyDbContext Context)
+        {
+            _Context = Context;
+            DepartmentRepository = new DepartmentRepository(Context);
+            EmployeeRepository = new EmployeeRepository(Context);   
+        }
+        public IDepartmentRepository DepartmentRepository { get  ; set ; }
+        public IEmployeeRepository EmployeeRepository { get; set; }
+       
+
+        public int Complete()
+           => _Context.SaveChanges();
+        
+    }
+}
--- Company.Web/Controllers/DepartmentController.cs
+++ Company.Web/Controllers/DepartmentController.cs
@@ -1,19 +1,86 @@
-﻿using Company.Repository.Interfaces;
+﻿using Company.Data.Entities;
+using Company.Repository.Interfaces;
+using Company.Service.Interfaces;
+using Company.Service.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Web.Controllers
 {
     public class DepartmentController : Controller
     {
-        private readonly IDepartmentRepository _departmentRepository;
-        public DepartmentController(IDepartmentRepository departmentRepository)
+        private readonly IDepartmentService _departmentService;
+        public DepartmentController(IDepartmentService departmentService)
         {
-            _departmentRepository = departmentRepository;
+            _departmentService = departmentService;
         }
         public IActionResult Index()
         {
-            var departments = _departmentRepository.GetAll();
+            var departments = _departmentService.GetAll();
             return View(departments);
         }
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+          
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _departmentService.Add(department);
+                    return RedirectToAction("Index");   //nameof(index)
+
+                }
+                ModelState.AddModelError("DepartmentError", "ValidationErrors");
+                return View(department);
+            }
+            catch (Exception ex )
+            {
+
+                ModelState.AddModelError("DepartmentError", ex.Message);
+                return View(department);
+            }
+        }
+
+        public IActionResult Details(int? id ,string viewName = "Details")
+        {
+            var department = _departmentService.GetById(id);
+            if (department == null)
+            {
+                return RedirectToAction("NotFoundPage" , null , "Home");
+            }
+            return View(viewName, department);
+        }
+        [HttpGet]
+        public IActionResult Update(int? id)
+        {
+          
+            return Details(id , "Update");
+        }
+        [HttpPost]
+        public IActionResult Update(int ? id , Department department)
+        {
+            if (department.Id != id.Value)
+                return RedirectToAction("NotFoundPage", null, "Home");
+            _departmentService.Update(department);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id) 
+        {
+            var department = _departmentService.GetById(id);
+            if (department == null)
+            {
+                return RedirectToAction("NotFoundPage", null, "Home");
+            }
+            _departmentService.Delete(department);
+            return RedirectToAction("Index");
+
+        }
+
+
     }
 }
